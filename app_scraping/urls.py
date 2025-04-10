aa5658355d11765f5ef9ab6457e234848fd1d933
Mario Medina <mariomedinam@gmail.com>
2025-04-10 10:03:03 -0500

    Agregar la ruta 'login'

--- app_scraping/urls.py
+++ app_scraping/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomeView, test_scraping
+from .views import HomeView, test_scraping, login_siaf
 
 urlpatterns = [
     path('home', HomeView.as_view(), name='home'),
     path('test-scraping', test_scraping.as_view(), name='test-scraping'),
+    path('login-siaf', login_siaf.as_view(), name='login-siaf'),
 ]
